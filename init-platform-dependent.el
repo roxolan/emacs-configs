352a6ff933fba4d8a6b37619e3e804db07f537e9
Edward Knyshov <edvorg@gmail.com>
2013-02-07 23:32:37 +0700

    arch linux specific config

--- init-platform-dependent.el
+++ init-platform-dependent.el
@@ -1,4 +1,19 @@
 (defun init-platform-dependent-start ()
+  ;; headers paths for clang completion
+
+  (setq ac-clang-flags 
+	(mapcar (lambda (item) (concat "-I" item))
+		(split-string
+		 "
+/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/../../../../include/c++/4.7.2
+/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/../../../../include/c++/4.7.2/x86_64-unknown-linux-gnu
+/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/../../../../include/c++/4.7.2/backward
+/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/include
+/usr/local/include
+/usr/lib/gcc/x86_64-unknown-linux-gnu/4.7.2/include-fixed
+/usr/include
+"
+		 )))
 )
 
 (provide 'init-platform-dependent)
