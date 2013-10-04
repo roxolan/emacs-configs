9fe4d34df95584de9fe3cafe37adf56cded19612
Edward Knyshov <edvorg@gmail.com>
2013-10-04 11:13:40 +0700

    clang flags for mac

--- clang-include-paths.sh
+++ clang-include-paths.sh
@@ -5,6 +5,7 @@ OUTPUT_SIZE=`cat tmp.txt | wc -l`
 UPPER_PATTERN='#include <...> search starts here:'
 BOTTOM_PATTERN='End of search list'
 
-cat tmp.txt | grep -A $OUTPUT_SIZE "$UPPER_PATTERN" | grep -B $OUTPUT_SIZE "$BOTTOM_PATTERN" | grep -v "$UPPER_PATTERN" | grep -v "$BOTTOM_PATTERN"
+cat tmp.txt | grep -A $OUTPUT_SIZE "$UPPER_PATTERN" | grep -B $OUTPUT_SIZE "$BOTTOM_PATTERN" | grep -v "$UPPER_PATTERN" | grep -v "$BOTTOM_PATTERN" | grep -vi "(framework directory)"
+cat tmp.txt | grep -A $OUTPUT_SIZE "$UPPER_PATTERN" | grep -B $OUTPUT_SIZE "$BOTTOM_PATTERN" | grep -v "$UPPER_PATTERN" | grep -v "$BOTTOM_PATTERN" | grep -i "(framework directory)" | while read line ; do echo ${line:0:${#line}-22} ; done
 
 rm tmp.txt
