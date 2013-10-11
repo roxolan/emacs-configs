4c7b6c5431090e41d306e1db0fb639f3a834c176
Edward Knyshov <edvorg@gmail.com>
2013-10-11 14:07:18 +0700

    use pbcopy on mac

--- init-platform-dependent-darwin.el
+++ init-platform-dependent-darwin.el
@@ -1,5 +1,7 @@
 (defun init-platform-dependent-start ()
   (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+  (require 'pbcopy)
+  (turn-on-pbcopy)
 )
 
 (provide 'init-platform-dependent-darwin)
