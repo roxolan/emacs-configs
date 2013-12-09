e3a361f23a9e3ebc2fd3d13ce9c1f88e40359ad4
Edward Knyshov <edvorg@gmail.com>
2013-12-09 18:43:36 +0700

    use path from shell to execute apps on OSX

--- init-platform-dependent-darwin.el
+++ init-platform-dependent-darwin.el
@@ -3,6 +3,9 @@
   (use-package pbcopy
 	:ensure pbcopy
 	:init (turn-on-pbcopy))
+  (use-package exec-path-from-shell
+	:ensure exec-path-from-shell
+	:init (progn (exec-path-from-shell-initialize)))
   )
 
 (provide 'init-platform-dependent-darwin)
