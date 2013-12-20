f0e528f8f222b42c56996447734562a77a918507
Edward Knyshov <edvorg@gmail.com>
2013-12-20 08:03:45 +0700

    use god mode

--- init-real.el
+++ init-real.el
@@ -450,6 +450,24 @@
   :init (progn (global-diff-hl-mode 1)
                (add-hook 'dired-mode-hook (lambda () (diff-hl-dired-mode 1)))))
 
+;; god mode
+
+(defun my-update-cursor ()
+  (setq cursor-type (if (or god-local-mode buffer-read-only) 'box 'bar)))
+
+(use-package god-mode
+  :ensure god-mode
+  :init (progn (global-set-key (kbd "<escape>") 'god-mode)
+			   (global-set-key (kbd "C-x C-1") 'delete-other-windows)
+			   (global-set-key (kbd "C-x C-2") 'split-window-below)
+			   (global-set-key (kbd "C-x C-3") 'split-window-right)
+			   (global-set-key (kbd "C-x C-0") 'delete-window)
+			   (define-key god-local-mode-map (kbd ".") 'repeat)
+			   (define-key god-local-mode-map (kbd "z") 'repeat)
+			   (define-key god-local-mode-map (kbd "i") 'god-mode)
+			   (add-hook 'god-mode-enabled-hook 'my-update-cursor)
+			   (add-hook 'god-mode-disabled-hook 'my-update-cursor)))
+
 ;; shrink/enlarge window
 
 (global-set-key (kbd "s-{") (lambda () (interactive) (shrink-window 10 1)))
