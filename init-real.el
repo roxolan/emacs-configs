49065bc9dd8497a704875c188d9039d2f3237198
Edward Knyshov <edvorg@gmail.com>
2013-12-30 22:14:41 +0700

    install req-package if not installed

--- init-real.el
+++ init-real.el
@@ -1,7 +1,24 @@
 ;; req-package
 
 (eval-when-compile (package-initialize))
-(require 'req-package)
+
+(require 'cl)
+
+(defun package-try-install (package)
+  "installs package if not installed"
+  (let* ((ARCHIVES (if (null package-archive-contents)
+                       (progn (package-refresh-contents)
+                              package-archive-contents)
+                     package-archive-contents))
+         (AVAIL (some (lambda (elem)
+                        (eq (car elem) package))
+                      ARCHIVES)))
+    (if AVAIL
+        (package-install package))))
+
+(if (null (require 'req-package "req-package" t))
+    (progn (package-try-install 'req-package)
+           (require 'req-package)))
 
 ;; save history
 
@@ -259,8 +276,8 @@
 ;; init.d
 
 (use-package load-dir
-             :ensure load-dir
-             :init (load-dir-one "~/.emacs.d/init.d"))
+  :ensure load-dir
+  :init (load-dir-one "~/.emacs.d/init.d"))
 
 ;; finish loading packages
 
