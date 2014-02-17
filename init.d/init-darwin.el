b0091ed7059e3eff5a84a6ce4af1eec590f8920a
Eduard Knyshov <eduardk@imac-eduard.zeptodev.local>
2014-02-17 17:32:22 +0400

    - pbcopy

--- init.d/init-darwin.el
+++ init.d/init-darwin.el
@@ -1,10 +1,6 @@
 (if (eq system-type 'darwin)
 	(progn   (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
 
-			 (req-package pbcopy
-                          :init
-                          (turn-on-pbcopy))
-
 			 (req-package exec-path-from-shell
                           :init
                           (exec-path-from-shell-initialize))))
