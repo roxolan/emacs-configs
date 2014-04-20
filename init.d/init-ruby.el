9704f1ce4423a8fd7a3e4d53ef69ac5a6c280493
Edward Knyshov <edvorg@gmail.com>
2014-04-20 12:50:31 +0400

    use ruby mode for Rakefile

--- init.d/init-ruby.el
+++ init.d/init-ruby.el
@@ -3,4 +3,9 @@
 (req-package rinari
   :require (ruby-compilation inf-ruby))
 
+;;
+
+(req-package ruby-mode
+  :init (add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode)))
+
 (provide 'init-ruby)
