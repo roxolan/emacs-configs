096b060febf9aaaf71c1cc9fd21dc57896b1b274
Edward Knyshov <edvorg@gmail.com>
2014-05-12 14:06:25 +0400

    tweak android-mode

--- init.d/init-android.el
+++ init.d/init-android.el
@@ -1,5 +1,6 @@
 ;; package for android sdk/ndk toolchain 
 
-(req-package android-mode)
+(req-package android-mode
+  :init (setq android-mode-sdk-dir (getenv "ANDROID_SDK")))
 
 (provide 'init-android)
