797f2403311748a83c4cfd7968c84266860dbb33
Edward Knyshov <edvorg@gmail.com>
2014-05-16 18:05:41 +0400

    simplify clang-include-paths.scala

--- clang-include-paths.scala
+++ clang-include-paths.scala
@@ -3,13 +3,10 @@ exec scala -savecompiled -feature "$0" "$@"
 !#
 
 import scala.sys.process._
-import scala.collection.mutable.ArrayBuffer
 
-val lines = ArrayBuffer[String]()
-
-"echo" #| "clang++ -v -E -x c++ -" ! ProcessLogger(_ => Unit, lines += _)
-
-val filtered = lines.takeWhile(!_.contains("End of search list")).reverse
+def process(in: java.io.InputStream): Unit = scala.io.Source.fromInputStream(in).getLines
+  .toList.takeWhile(!_.contains("End of search list")).reverse
   .takeWhile(!_.contains("#include <...> search starts here:"))
+  .map(x => println("\\(framework directory\\)".r.replaceAllIn(x, "")))
 
-filtered.map(x => println("\\(framework directory\\)".r.replaceAllIn(x, "")))
+"echo" #| "clang++ -v -E -x c++ -" run new ProcessIO(_.close(), _.close(), process _)
