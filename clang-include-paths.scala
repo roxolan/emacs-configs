2bc160946eb740e6b14a067501928557df0e1f12
Edward Knyshov <edvorg@gmail.com>
2014-05-16 18:51:36 +0400

    simplify clang-include-paths.scala

--- clang-include-paths.scala
+++ clang-include-paths.scala
@@ -4,9 +4,8 @@ exec scala -savecompiled -feature "$0" "$@"
 
 import scala.sys.process._
 
-def process(in: java.io.InputStream): Unit = scala.io.Source.fromInputStream(in).getLines
-  .toList.takeWhile(!_.contains("End of search list")).reverse
-  .takeWhile(!_.contains("#include <...> search starts here:"))
-  .map(x => println("\\(framework directory\\)".r.replaceAllIn(x, "")))
-
-"echo" #| "clang++ -v -E -x c++ -" run new ProcessIO(_.close(), _.close(), process _)
+"echo" #| "clang++ -v -E -x c++ -" run new ProcessIO(_.close(), _.close(), in =>
+  scala.io.Source.fromInputStream(in).getLines.toList
+	.takeWhile(!_.contains("End of search list")).reverse
+	.takeWhile(!_.contains("#include <...> search starts here:"))
+	.foreach(x => println("\\(framework directory\\)".r.replaceAllIn(x, ""))))
