2734fd54a2ff969771859316e9d43ab6c8d72d78
Edward Knyshov <edvorg@gmail.com>
2014-06-07 15:44:09 +0400

    - dired single

--- init.d/init-dired.el
+++ init.d/init-dired.el
@@ -1,28 +1,12 @@
 ;; single dired
 
-(req-package dired-single
+(req-package dired
   :require
-  (dired helm-swoop autorevert diff-hl)
+  (helm-swoop autorevert diff-hl)
   :init
-  (progn (define-key dired-mode-map (kbd "f")
-           'dired-single-buffer)
-
-         (define-key dired-mode-map (kbd "<RET>")
-           'dired-single-buffer)
-
-         (define-key dired-mode-map (kbd "^")
-           (function (lambda ()
-                       (interactive)
-                       (dired-single-buffer ".."))))
-
-         (define-key dired-mode-map (kbd "M-i")
-           'helm-swoop)
-
-         (add-hook 'dired-mode-hook (lambda ()
-                                      (auto-revert-mode 1)))
-
-         (add-hook 'dired-mode-hook (lambda ()
-                                      (diff-hl-dired-mode 1)))))
+  (progn (define-key dired-mode-map (kbd "M-i") 'helm-swoop)
+         (add-hook 'dired-mode-hook (lambda () (auto-revert-mode 1)))
+         (add-hook 'dired-mode-hook (lambda () (diff-hl-dired-mode 1)))))
 
 ;; sunrise commander
 
