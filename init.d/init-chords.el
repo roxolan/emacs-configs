dcfc9bd70fe0a6725f8900a7f86719f569fa02fb
Edward Knyshov <edvorg@gmail.com>
2014-06-13 19:10:28 +0400

    + magit chord

--- init.d/init-chords.el
+++ init.d/init-chords.el
@@ -8,24 +8,27 @@
 			   (key-chord-define-global "gg" 'keyboard-quit)
 			   (key-chord-define-global "gm" 'god-mode)
 			   (key-chord-define-global "xb" 'helm-buffers-list)
-			   (key-chord-define-global "3e" "\C-e")
+			   (key-chord-define-global ";m" 'magit-status)
+			   (key-chord-define-global "3e" "\C-e ")
 			   (key-chord-define-global "[]" "[]\C-b")
 			   (key-chord-define-global "<>" "<>\C-b")
 			   (key-chord-define-global "''" "''\C-b")
 			   (key-chord-define-global "\"\"" "\"\"\C-b")
+			   (key-chord-define-global "()" "()\C-b")
 
 			   (key-chord-define minibuffer-local-map "gg" 'abort-recursive-edit)
 
-			   (key-chord-define emacs-lisp-mode-map "()" "()\C-b")
-
 			   (key-chord-define dired-mode-map "qq" 'dired-up-directory)
 
+			   (key-chord-define scala-mode-map ";;"  "\C-e;")
 			   (key-chord-define scala-mode-map "//"  "\C-e //")
 			   (key-chord-define scala-mode-map "{}"  "{\n\n}\C-p\t")
+			   (key-chord-define scala-mode-map "(}"  "() {\n\n}\C-p\t")
 
 			   (key-chord-define c++-mode-map ";;"  "\C-e;")
 			   (key-chord-define c++-mode-map "//"  "\C-e //")
 			   (key-chord-define c++-mode-map "{}"  "{\n\n}\C-p\t")
+			   (key-chord-define c++-mode-map "(}"  "() {\n\n}\C-p\t")
 
 			   (key-chord-mode 1)))
 
