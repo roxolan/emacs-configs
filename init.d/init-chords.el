08b1dd87e0593b8a9811e0eb55820d78b61287f0
Edward Knyshov <edvorg@gmail.com>
2014-06-13 19:53:54 +0400

    + chords

--- init.d/init-chords.el
+++ init.d/init-chords.el
@@ -1,7 +1,7 @@
 ;; key chord mode
 
 (req-package key-chord
-  :require (dired cc-mode god-mode)
+  :require (dired cc-mode god-mode smex helm magit scala-mode2)
   :init (progn (key-chord-define-global "qq" 'kill-this-buffer)
 			   (key-chord-define-global "qw" 'kill-this-buffer)
 			   (key-chord-define-global "xm" 'smex)
@@ -10,12 +10,25 @@
 			   (key-chord-define-global "xb" 'helm-buffers-list)
 			   (key-chord-define-global "xs" 'save-buffer)
 			   (key-chord-define-global "xc" 'save-buffers-kill-emacs)
+
 			   (key-chord-define-global ";m" 'magit-status)
+			   (key-chord-define-global ";l" 'helm-bookmarks)
+			   (key-chord-define-global ";p" 'paradox-list-packages)
+			   (key-chord-define-global ";j" 'open-line)
+			   (key-chord-define-global ";k" 'kill-line)
+			   (key-chord-define-global ";s" 'isearch-forward)
+			   (key-chord-define-global ";r" 'isearch-forward-regexp)
+
 			   (key-chord-define-global ";o" 'switch-window)
 			   (key-chord-define-global ";1" 'delete-other-windows)
 			   (key-chord-define-global ";2" 'split-window-below)
 			   (key-chord-define-global ";3" 'split-window-right)
+
+			   (key-chord-define-global "1e" "\C-a")
+			   (key-chord-define-global "2e" "\C-a\t")
 			   (key-chord-define-global "3e" "\C-e ")
+			   (key-chord-define-global "4e" "\C-e")
+
 			   (key-chord-define-global "[]" "[]\C-b")
 			   (key-chord-define-global "<>" "<>\C-b")
 			   (key-chord-define-global "''" "''\C-b")
