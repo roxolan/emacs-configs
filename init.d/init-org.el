1a8e58c6cb41046f41ee651b44fc9dbeb9e805f4
Edward Knyshov <edvorg@gmail.com>
2014-06-20 14:07:49 +0400

    optimize org

--- init.d/init-org.el
+++ init.d/init-org.el
@@ -1,12 +1,10 @@
 (req-package org
-  :config
-  (progn (global-set-key "\C-cl" 'org-store-link)
-         (global-set-key "\C-cc" 'org-capture)
-         (global-set-key "\C-ca" 'org-agenda)
-         (global-set-key "\C-cb" 'org-iswitchb)))
+  :mode ("\\.org$" . org-mode)
+  :bind (("C-c l" . org-store-link)
+         ("C-c c" . org-capture)
+         ("C-c a" . org-agenda)
+         ("C-c b" . org-iswitchb)))
 
-(req-package org-gcal
-  :disabled t
-  :require org)
+(req-package org-gcal :disabled t :require org)
 
 (provide 'init-org)
