f33796f759707561cef3cbea67c1505a100dc41d
Edward Knyshov <edvorg@gmail.com>
2014-06-20 13:38:14 +0400

    fix mode alist for glsl mode

--- init.d/init-glsl.el
+++ init.d/init-glsl.el
@@ -1,9 +1,9 @@
 ;; glsl
 
 (req-package glsl-mode
-  :mode "\\.vs\\'"
-  :mode "\\.fs\\'"
-  :mode "\\.gs\\'"
+  :mode (("\\.vs\\'" . glsl-mode)
+         ("\\.fs\\'" . glsl-mode)
+         ("\\.gs\\'" . glsl-mode))
   :config (setq glsl-other-file-alist '(("\\.fs$" (".vs"))
                                         ("\\.vs$" (".fs")))))
 
