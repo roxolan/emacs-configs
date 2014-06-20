3755ff3cd5025211108a8c8af7d9eb24efb4149f
Edward Knyshov <edvorg@gmail.com>
2014-06-20 14:33:06 +0400

    optimize elnode

--- init.d/init-web.el
+++ init.d/init-web.el
@@ -9,11 +9,12 @@
 
 ;; elnode
 
-(req-package elnode)
+(req-package elnode :commands (elnode-stop elnode-start))
 
 ;; pick mode
 
 (req-package peek-mode
+  :commands peek-mode
   :require elnode
   :config (elnode-start 'peek-mode-dispatcher-handler :port 8008 :host "localhost"))
 
