a6c18c15a5718a6abeedae66141a22b2ff37f03d
Edward Knyshov <edvorg@gmail.com>
2014-06-22 16:54:43 +0400

    optimize haskell mode

--- init.d/init-haskell.el
+++ init.d/init-haskell.el
@@ -1,6 +1,7 @@
 ;; haskell mode
 
 (req-package haskell-mode
+  :mode "\\.hs$"
   :config (progn (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
                  (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)))
 
