9d74b6eb75c704de10261d077c2e47f3ecb7f690
Edward Knyshov <edvorg@gmail.com>
2014-11-09 21:17:35 +0300

    refactor archvis config

--- init.el
+++ init.el
@@ -1,11 +1,22 @@
 (require 'package)
 
-(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("elpa" . "http://tromey.com/elpa/")
-                         ("org" . "http://orgmode.org/elpa/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
-                         ("sunrise" . "http://joseito.republika.pl/sunrise-commander/")))
+(defconst emacs-major-version-rad 1000000)
+
+(defun has-emacs-version (major minor)
+  "will be broken when emacs marjor version will jump over emacs-major-version-rad"
+  (<= (+ (* major emacs-major-version-rad) minor)
+      (+ (* emacs-major-version emacs-major-version-rad) emacs-minor-version)))
+
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("elpa" . "http://tromey.com/elpa/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("sunrise" . "http://joseito.republika.pl/sunrise-commander/"))
+
+(if (not (has-emacs-version 24 0))
+    (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+
+(setq package-enable-at-startup nil)
 
 (defconst my-custom-file "~/.emacs.d/custom.el")
 (defconst my-preload-file "~/.emacs.d/init-preload.el")
