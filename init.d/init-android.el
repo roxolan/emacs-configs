e8db90b0d557228a0e32f03f2a6db202bc7293c4
Edward Knyshov <edvorg@gmail.com>
2014-11-17 02:51:11 +0300

    disable android mode

--- init.d/init-android.el
+++ init.d/init-android.el
@@ -1,6 +1,7 @@
-;; package for android sdk/ndk toolchain 
+;; package for android sdk/ndk toolchain
 
 (req-package android-mode
+  :disabled t
   :config (setq android-mode-sdk-dir (getenv "ANDROID_SDK")))
 
 (provide 'init-android)
