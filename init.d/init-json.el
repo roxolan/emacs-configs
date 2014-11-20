feaef0f7aef6d9f9400864e72741bda83dd40b85
Edward Knyshov <edvorg@gmail.com>
2014-11-20 15:53:05 +0300

    + json mode, flymake json

--- init.d/init-json.el
+++ init.d/init-json.el
@@ -2,4 +2,12 @@
 
 (req-package json-reformat :commands json-reformat-region)
 
+(req-package flymake-json
+  :require flymake)
+
+(req-package json-mode
+  :require flymake-json
+  :mode ("\\.json$" . json-mode)
+  :init (add-hook 'json-mode-hook (lambda () (flymake-json-load))))
+
 (provide 'init-json)
