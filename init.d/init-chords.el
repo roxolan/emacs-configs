fd485133d48cdf09dd36d1db57d2a0f64bfb92d9
Edward Knyshov <edvorg@gmail.com>
2015-03-25 11:12:49 +0600

    cleanup chords

--- init.d/init-chords.el
+++ init.d/init-chords.el
@@ -3,7 +3,6 @@
 (req-package key-chord
   :config (progn (key-chord-define-global "qw" 'kill-this-buffer)
                  (key-chord-define-global "qq" 'kill-this-buffer)
-                 (key-chord-define-global ";r" 'isearch-forward-regexp)
                  (key-chord-define-global ";2" 'split-window-below)
                  (key-chord-define-global ";3" 'split-window-right)
                  (key-chord-define-global ";4" 'kill-buffer-and-window)
@@ -11,11 +10,6 @@
                  (key-chord-define-global "2e" "\C-a\t")
                  (key-chord-define-global "3e" "\C-e")
                  (key-chord-define-global "4e" "\C-e ")
-                 (key-chord-define-global "[]" "[]\C-b")
-                 (key-chord-define-global "<>" "<>\C-b")
-                 (key-chord-define-global "''" "''\C-b")
-                 (key-chord-define-global "\"\"" "\"\"\C-b")
-                 (key-chord-define-global "()" "()\C-b")
                  (key-chord-mode 1)))
 
 (provide 'init-chords)
