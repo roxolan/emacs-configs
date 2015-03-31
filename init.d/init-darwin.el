51b02f3c32c4abe494d97347c38bff4876232a77
Edward Knyshov <edvorg@gmail.com>
2015-03-31 12:05:41 +0300

    tweak fonts on mac

--- init.d/init-darwin.el
+++ init.d/init-darwin.el
@@ -6,6 +6,9 @@
 		   (setenv "ANDROID_SDK" "/usr/local/opt/android-sdk")
 		   (setenv "ANDROID_HOME" "/usr/local/opt/android-sdk")
 		   (setenv "ANDROID_NDK" "/usr/local/opt/android-ndk")
-           (add-to-list 'exec-path "/usr/local/bin")))
+           (add-to-list 'exec-path "/usr/local/bin")
+
+           (set-default-font "SourceCodePro-12" t)
+           (set-fontset-font "fontset-default" 'cyrillic-iso8859-5 '("Monaco-12" . "iso10646-1"))))
 
 (provide 'init-darwin)
