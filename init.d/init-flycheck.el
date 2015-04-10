faa55f6b3c117f808f65a90f53ad2ff7762e1a8d
Edward Knyshov <edvorg@gmail.com>
2015-04-10 23:46:28 +0300

    use flycheck-pos-tip

--- init.d/init-flycheck.el
+++ init.d/init-flycheck.el
@@ -1,4 +1,9 @@
 (req-package flycheck
   :config (global-flycheck-mode 1))
 
+(req-package flycheck-pos-tip
+  :comments flycheck-pos-tip-error-messages
+  :require flycheck
+  :init (setq flycheck-display-errors-function #'flycheck-pos-tip-error-messages))
+
 (provide 'init-flycheck)
