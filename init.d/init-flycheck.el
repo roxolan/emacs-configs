f16d6387bf73e8028f012d8444955f2ecb44e808
Edward Knyshov <edvorg@gmail.com>
2015-04-18 21:55:42 +0300

    tweak flycheck faces

--- init.d/init-flycheck.el
+++ init.d/init-flycheck.el
@@ -1,5 +1,11 @@
 (req-package flycheck
-  :config (global-flycheck-mode 1))
+  :config (progn (global-flycheck-mode 1)
+                 (set-face-attribute 'flycheck-warning nil
+                                     :inherit 'warning
+                                     :underline nil)
+                 (set-face-attribute 'flycheck-error nil
+                                     :inherit 'error
+                                     :underline nil)))
 
 (req-package flycheck-pos-tip
   :commands flycheck-pos-tip-error-messages
