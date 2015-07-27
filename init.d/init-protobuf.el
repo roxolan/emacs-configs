5270a2cdbe0a879c14643fd9e24071946fbbc050
Edward Knyshov <edvorg@gmail.com>
2015-07-27 16:04:47 +0300

    protoc flycheck

--- init.d/init-protobuf.el
+++ init.d/init-protobuf.el
@@ -1,4 +1,19 @@
 (require 'req-package)
 
+(flycheck-define-checker protobuf-protoc-reporter
+  "A protobuf syntax checker based on protoc compiler"
+  :command ("/Users/eduardk/.emacs.d/check-protoc.sh" source-inplace)
+  :error-patterns
+  ((error line-start
+          (message "In file included from") " " (file-name) ":" line ":"
+          column ":"
+          line-end)
+   (info line-start (file-name) ":" line ":" column
+         ": note: " (message) line-end)
+   (error line-start (file-name) ":" line ":" column
+          ": " (message) line-end))
+  :modes (protobuf-mode))
+
 (req-package protobuf-mode
-  :mode (("proto\\'" . protobuf-mode)))
+  :mode (("proto\\'" . protobuf-mode))
+  :config (add-hook 'protobuf-mode-hook (lambda () (flycheck-select-checker 'protobuf-protoc-reporter))))
