dc542951e8212f06514caf04d167f36a7b52765f
Edward Knyshov <edvorg@gmail.com>
2015-07-31 11:03:36 +0300

    fix protobuf integration

--- init.d/init-protobuf.el
+++ init.d/init-protobuf.el
@@ -1,19 +1,23 @@
 (require 'req-package)
 
-(flycheck-define-checker protobuf-protoc-reporter
-  "A protobuf syntax checker based on protoc compiler"
-  :command ("/Users/eduardk/.emacs.d/check-protoc.sh" source-inplace)
-  :error-patterns
-  ((error line-start
-          (message "In file included from") " " (file-name) ":" line ":"
-          column ":"
-          line-end)
-   (info line-start (file-name) ":" line ":" column
-         ": note: " (message) line-end)
-   (error line-start (file-name) ":" line ":" column
-          ": " (message) line-end))
-  :modes (protobuf-mode))
-
 (req-package protobuf-mode
+  :require flycheck
   :mode (("proto\\'" . protobuf-mode))
-  :config (add-hook 'protobuf-mode-hook (lambda () (flycheck-select-checker 'protobuf-protoc-reporter))))
+  :config
+  (progn
+    (flycheck-define-checker protobuf-protoc-reporter
+      "A protobuf syntax checker based on protoc compiler"
+      :command ("protoc" "--error_format" "gcc" "--java_out=/tmp" "--proto_path" source-directory source-inplace)
+      :error-patterns
+      ((error line-start
+              (message "In file included from") " " (file-name) ":" line ":"
+              column ":"
+              line-end)
+       (info line-start (file-name) ":" line ":" column
+             ": note: " (message) line-end)
+       (error line-start (file-name) ":" line ":" column
+              ": " (message) line-end))
+      :modes (protobuf-mode))
+    (add-hook 'protobuf-mode-hook (lambda () (flycheck-select-checker 'protobuf-protoc-reporter)))))
+
+(provide 'init-protobuf)
