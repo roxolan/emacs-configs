a721c3d00b059c2925109857c2232656bab9b865
Edward Knyshov <edvorg@gmail.com>
2015-08-13 12:50:37 +0300

    fix protobuf checker

--- init.d/init-protobuf.el
+++ init.d/init-protobuf.el
@@ -1,11 +1,8 @@
 (require 'req-package)
 
-(req-package flycheck-protobuf
-  :require flycheck
-  :config (add-to-list 'flycheck-checkers 'protobuf-protoc-reporter t))
-
 (req-package protobuf-mode
-  :require flycheck-protobuf
-  :mode(("proto\\'" . protobuf-mode)))
+  :require (flycheck flycheck-protobuf)
+  :mode(("proto\\'" . protobuf-mode))
+  :config (add-hook 'protobuf-mode-hook (lambda () (flycheck-select-checker 'protobuf-protoc-reporter))))
 
 (provide 'init-protobuf)
