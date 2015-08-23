1f5e836f8bf41abee04f84a2ec8c8a4b14e46ebf
Edward Knyshov <edvorg@gmail.com>
2015-08-23 23:45:22 +0300

    try flx ido

--- init.d/init-ido.el
+++ init.d/init-ido.el
@@ -1,15 +1,15 @@
 (require 'req-package)
 
-;; ido mode
-
-(req-package ido-better-flex
-  :require key-chord
+(req-package flx-ido
+  :require (flx key-chord)
   :config (progn (ido-mode 1)
-				 (ido-everywhere 1)
-				 (ido-better-flex/enable)
-				 (global-set-key (kbd "C-x C-f") 'ido-find-file)
-				 (key-chord-define-global ";f" 'ido-find-file)
-				 (key-chord-define-global "xb" 'ido-switch-buffer)))
+                 (ido-everywhere 1)
+                 (flx-ido-mode 1)
+                 ;; disable ido faces to see flx highlights.
+                 (setq ido-enable-flex-matching t)
+                 (setq ido-use-faces nil)
+                 (global-set-key (kbd "C-x C-f") 'ido-find-file)
+                 (key-chord-define-global "xb" 'ido-switch-buffer)))y
 
 (req-package ido-at-point
   :config (ido-at-point-mode 1))
