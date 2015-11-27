9485a6e6cf8f8e4af4de50b950afb60dc2b0e4ad
Edward Knyshov <edvorg@gmail.com>
2015-11-28 01:28:10 +0300

    remove android

--- init.d/init-android.el
+++
@@ -1,9 +0,0 @@
-(require 'req-package)
-
-;; package for android sdk/ndk toolchain
-
-(req-package android-mode
-  :disabled t
-  :config (setq android-mode-sdk-dir (getenv "ANDROID_SDK")))
-
-(provide 'init-android)
