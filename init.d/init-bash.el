cc0fffa81058c934067d9d196bbc8940e945597e
Edward Knyshov <edvorg@gmail.com>
2015-11-28 01:29:16 +0300

    switch off bash completion

--- init.d/init-bash.el
+++ init.d/init-bash.el
@@ -1,14 +1,16 @@
 (require 'req-package)
 
 (req-package bash-completion
+  :disabled t
   :require shell
   :commands bash-completion-dynamic-complete
-  :init (progn (setq explicit-shell-file-name "bash")
-			   (setq comint-process-echoes t)
-			   (req-package-hooks-add-execute-impl 'shell-mode 'shell-dynamic-complete-functions
-                 'bash-completion-dynamic-complete)
-			   (req-package-hooks-add-execute-impl 'shell-mode 'shell-command-complete-functions
-                 'bash-completion-dynamic-complete)))
+  :init
+  (setq explicit-shell-file-name "bash")
+  (setq comint-process-echoes t)
+  (req-package-hooks-add-execute-impl 'shell-mode 'shell-dynamic-complete-functions
+    'bash-completion-dynamic-complete)
+  (req-package-hooks-add-execute-impl 'shell-mode 'shell-command-complete-functions
+    'bash-completion-dynamic-complete))
 
 (req-package shell-pop
   :require key-chord
