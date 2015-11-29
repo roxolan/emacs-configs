86ae8365a9537446b91f262d606aef0afec28fff
Edward Knyshov <edvorg@gmail.com>
2015-11-30 01:00:14 +0300

    tweak fonts for linux

--- init.d/init-linux.el
+++ init.d/init-linux.el
@@ -1,21 +1,13 @@
 (require 'req-package)
 
-(if (eq system-type 'gnu/linux)
-
-	(progn ;; open links with xdg
-
-           (req-package browse-url
-             :config (setq browse-url-browser-function
-                           (lambda (url &rest args)
-                             (call-process-shell-command "xdg-open" nil 0 nil url))))
-
-           ;; fonts
-
-           ;; FIXME install this fonts somehow first
-           ;; (set-default-font "SourceCodePro-13" t)
-           ;; (set-fontset-font "fontset-default" 'cyrillic-iso8859-5 '("Liberation Mono" . "iso10646-1"))
-
-           ;; aur interface
-           (req-package aurel :commands aurel-package-search)))
+(when (eq system-type 'gnu/linux)
+  (req-package browse-url
+    :config (setq browse-url-browser-function
+                  (lambda (url &rest args)
+                    (call-process-shell-command "xdg-open" nil 0 nil url))))
+  ;; FIXME install this fonts somehow first
+  (set-default-font "SourceCodePro-9" t)
+  ;; aur interface
+  (req-package aurel :commands aurel-package-search))
 
 (provide 'init-linux)
